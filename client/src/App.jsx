0635e4ec48b6e6ffb0783bfc388e4a87ca6174f5
wwwwswe <rotey1016@gmail.com>
2023-07-07 17:42:03 +0900

    feat/FE/common/Input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
 import ErrorPage from "./pages/ErrorPage"
+import Test from "./pages/Test";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="*" element={<Test />} />
         <Route path="/profile/:id" element={<ProfilePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
