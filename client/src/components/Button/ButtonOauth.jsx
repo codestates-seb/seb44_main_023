6fa46ff3acd00e019e050b513de8c753a036b7cc
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-07 13:42:08 +0900

    fix : Outh Btn 하나로 합침

--- client/src/components/Button/ButtonOauth.jsx
+++ client/src/components/Button/ButtonOauth.jsx
@@ -5,69 +5,75 @@ import kakaoIcon from "../../assets/icons/kakao_icon.svg";
 import naverIcon from "../../assets/icons/naver_icon.svg";
 
 export default function ButtonOauth(props) {
-  const { google, kakao, naver, onGoogleClick, onKakaoClick, onNaverClick} = props;
+  const { onGoogleClick, onKakaoClick, onNaverClick } = props;
 
   return (
     <ButtonContainer>
-      {google && (
-        <GoogleButton onClick={onGoogleClick}>
-          <GoogleIcon />
-        </GoogleButton>
-      )}
-      {kakao && (
-        <KakaoButton onClick={onKakaoClick}>
-          <KakaoIcon />
-        </KakaoButton>
-      )}
-      {naver && (
-        <NaverButton onClick={onNaverClick}>
-          <NaverIcon/>
-        </NaverButton>
-      )}
+      <GoogleButton onClick={onGoogleClick}>
+        <GoogleIcon />
+      </GoogleButton>
+      <KakaoButton onClick={onKakaoClick}>
+        <KakaoIcon />
+      </KakaoButton>
+      <NaverButton onClick={onNaverClick}>
+        <NaverIcon />
+      </NaverButton>
     </ButtonContainer>
   );
 }
 
 const ButtonContainer = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 4rem;
 `;
 
 const GoogleButton = styled.button`
-  width: 4.1725rem;
-  height: 4.1725rem;
+  width: 6.676rem;
+  height: 6.676rem;
   border-radius: 50%;
   background-color: white;
   border: none;
   cursor: pointer;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+  &:hover {
+    filter: brightness(90%) drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
 `;
 
 const KakaoButton = styled.button`
-  width: 4.1725rem;
-  height: 4.1725rem;
+  width: 6.676rem;
+  height: 6.676rem;
   border-radius: 50%;
-  background-color: #FDDC3F;
+  background-color: #fddc3f;
   border: none;
   cursor: pointer;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+  &:hover {
+    filter: brightness(90%) drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
 `;
 
 const NaverButton = styled.button`
-  width: 4.1725rem;
-  height: 4.1725rem;
+  width: 6.676rem;
+  height: 6.676rem;
   border-radius: 50%;
-  background-color: #06BE34;
+  background-color: #02bf21;
   border: none;
   cursor: pointer;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+  &:hover {
+    filter: brightness(90%) drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
 `;
 
 const GoogleIcon = styled.div`
   position: absolute;
-  transform: translate(20%, -50%);
-  width: 2.5rem;
-  height: 2.5rem;
+  transform: translate(33%, -50%);
+  width: 4rem;
+  height: 4rem;
   background-image: url(${googleIcon});
   background-repeat: no-repeat;
   background-size: cover;
@@ -75,9 +81,9 @@ const GoogleIcon = styled.div`
 
 const KakaoIcon = styled.div`
   position: absolute;
-  transform: translate(-5%, -53%);
-  width: 3.9rem;
-  height: 3.9rem;
+  transform: translate(4%, -53%);
+  width: 6.24rem;
+  height: 6.24rem;
   background-image: url(${kakaoIcon});
   background-repeat: no-repeat;
   background-size: cover;
@@ -85,9 +91,9 @@ const KakaoIcon = styled.div`
 
 const NaverIcon = styled.div`
   position: absolute;
-  transform: translate(40%, -50%);
-  width: 1.9rem;
-  height: 1.9rem;
+  transform: translate(61%, -50%);
+  width: 3.04rem;
+  height: 3.04rem;
   background-image: url(${naverIcon});
   background-repeat: no-repeat;
   background-size: cover;
