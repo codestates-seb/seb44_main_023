3b9611666350d38611b57eb1104b044b715f5fe9
wwwwswe <rotey1016@gmail.com>
2023-07-07 16:08:32 +0900

    feat:FE/common/AmountInput 구현

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,63 +1,41 @@
 import React, { useState } from "react";
-import Input from "../components/Input/PageInput";
-import { FiSend } from "react-icons/fi";
 import styled from "styled-components";
-
-const TestContainer = styled.div`
-  background-color: #ffe7ba;
-  height: 500px;
-`;
+import AmountInput from "../components/Input/AmountInput";
 
 const TestComponent = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [comment, setComment] = useState("");
   const [amount, setAmount] = useState("");
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleContentChange = (event) => {
-    setContent(event.target.value);
-  };
-
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
-
-  const handleAmountChange = (event) => {
-    setAmount(event.target.value);
-  };
-
-  const handleCommentSubmit = () => {
-    console.log("댓글 내용:", comment);
+  const handleAmountChange = (value) => {
+    setAmount(value);
   };
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-      console.log("댓글 내용:", comment);
+      const numericValue = Number(amount.replace(/[^0-9.-]+/g, ""));
+      console.log("금액:", numericValue);
     }
   };
 
   return (
-    <TestContainer>
-      <Input
-        placeholder="제목을 입력하세요"
-        value={title}
-        width="20rem"
-        onChange={handleTitleChange}
-        info="이메일이 잘못되었습니다"
-      />
-      <Input
-        placeholder="댓글을 입력하세요"
-        value={comment}
-        width="20rem"
-        onChange={handleCommentChange}
+    <Test>
+      <AmountInput
+        placeholder="금액을 입력해주세요"
+        size="20rem"
+        height="90px"
+        fontSize="1.6rem"
+        value={amount}
+        onChange={handleAmountChange}
         onKeyPress={handleKeyPress}
+        textSize="12px"
+        fontWeight="600" // 폰트 굵기 프롭스 추가
       />
-    </TestContainer>
+    </Test>
   );
 };
+
+const Test = styled.div`
+  background-color: #ffedcc;
+  height: 500px;
+`;
 
 export default TestComponent;
