695b9f960cbc724248d75e44fe600bf2d698d016
jina <wlsdk7245@gmail.com>
2023-07-07 14:15:12 +0900

    feat: add profile page, router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage"
-import Test from "./pages/Test"
+import ErrorPage from "./pages/ErrorPage";
+import Test from "./pages/Test";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="*" element={<Test />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import background from "../assets/background.png";
+import ProfileTop from "../feature/Profile/ProfileTop";
+import ProfileBottom from "../feature/Profile/ProfileBottom";
+import Loading from "../components/Loading/Loading";
+
+const ProfilePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [profileInfo, setProfileInfo] = useState({});
+
+  const { id } = useParams();
+
+  const requestProfileInfo = async () => {
+    try {
+      const res = {
+        status: 200,
+        data: {
+          member_id: 1,
+          nickname: "닉네임",
+          email: "example@example.com",
+          profile_image:
+            "https://i.pinimg.com/474x/df/2d/25/df2d253fbd0eb7d50193f1374128e9f0.jpg",
+        },
+      };
+
+      // TODO 프로필 정보 받아오기 API 연결
+      if (res.status !== 200) throw res;
+
+      setProfileInfo(res.data);
+      setTimeout(() => setIsLoading(false), 2000);
+    } catch (err) {
+      console.log("error 발생", err);
+    }
+  };
+
+  useEffect(() => {
+    requestProfileInfo();
+  }, []);
+
+  return (
+    <P>
+      <StyledWrapper>
+        <ProfileBox>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <>
+              <ProfileTop profileInfo={profileInfo} />
+              <ProfileBottom profileInfo={profileInfo} />
+            </>
+          )}
+        </ProfileBox>
+      </StyledWrapper>
+    </P>
+  );
+};
+
+export default ProfilePage;
+
+const P = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+`;
+
+const StyledWrapper = styled.div`
+  padding: 6.4rem 6.4rem 0;
+`;
+
+const ProfileBox = styled.div`
+  padding: 6.4rem;
+  background-color: var(--color-gray-02);
+  min-height: calc(100vh - 19.2rem);
+  border-radius: 1rem 1rem 0 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: var(--color-gray-06);
+`;
