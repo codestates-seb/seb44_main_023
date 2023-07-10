554b10999970fb69386f672dcd95f046cf9e9a93
wwwwswe <rotey1016@gmail.com>
2023-07-10 21:53:50 +0900

    feat:FE/Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
-import ErrorPage from "./pages/ErrorPage"
+import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/profile/:id" element={<ProfilePage />} />
         <Route path="*" element={<ErrorPage />} />
