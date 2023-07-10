7e2373cdfb0dd9f4fc68142394a60f3aebbd6437
yunee <joanaun92@gmail.com>
2023-07-10 21:09:04 +0900

    design : weather widget design

--- client/src/pages/TestWidget.jsx
+++ client/src/pages/TestWidget.jsx
@@ -2,8 +2,19 @@ import WeatherWidget from "../components/WeatherWidget/WeatherWidget"
 const TestWidget = () => {
 
 return(
-<WeatherWidget></WeatherWidget>    
-);
+<div>
+      <h1>Weather Widget Example</h1>
+
+      <div>
+        <h2>Normal Size</h2>
+        <WeatherWidget />
+      </div>
+
+      <div>
+        <h2>70% Scale</h2>
+        <WeatherWidget scale={0.7} />
+      </div>
+    </div>);
 
 }
 export default TestWidget;
