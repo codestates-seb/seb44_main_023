f44ae8cd8f07438818f9bab59e23f9afc0262f31
whsg0245@gmail.com <whsg0245@gmail.com>
2023-07-12 10:32:14 +0900

    chore : CORS Error Handling

---
+++ server/src/main/java/com/main/server/auth/AuthController.java
@@ -0,0 +1,122 @@
+package com.main.server.auth;
+
+import com.main.server.auth.dto.AuthDto;
+import com.main.server.auth.dto.AuthResponse;
+import com.main.server.security.JwtTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+
+// CORS Handle
+@CrossOrigin
+@RestController
+public class AuthController {
+
+    private AuthService authService;
+    private JwtTokenizer jwtTokenizer;
+
+    private Set<String> tokenBlacklist = new HashSet<>();
+
+    @Autowired
+    public AuthController(AuthService authService,
+                          JwtTokenizer jwtTokenizer) {
+        this.authService = authService;
+        this.jwtTokenizer = jwtTokenizer;
+
+    }
+
+//    @PostMapping("/auths")
+//    public ResponseEntity<AuthResponse> postAuth(@Valid @RequestBody AuthDto authDto) {
+//
+//        String email = authDto.getEmail();
+//        String password = authDto.getPassword();
+//
+//        authService.authenticate(email, password);
+//
+//        String accessToken = jwtTokenizer.generateAccessToken(email);
+//        String refreshToken = jwtTokenizer.generateRefreshToken(email);
+//
+//        AuthResponse authResponse = new AuthResponse(accessToken, refreshToken);
+//
+//        return ResponseEntity.ok(authResponse);
+//    }
+
+    @PostMapping("/auths")
+    public ResponseEntity<?> postAuth(@Valid @RequestBody AuthDto authDto,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // 유효성 검증에 실패한 경우 에러 메시지를 처리하고 응답을 구성한다.
+            StringBuilder errorMessage = new StringBuilder();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMessage.append(error.getField())
+                        .append(": ")
+                        .append(error.getDefaultMessage())
+                        .append("; \n");
+            }
+            // 에러 메시지를 포스트맨으로 보내기 위해 ResponseEntity를 사용한다.
+            return ResponseEntity.badRequest().body(errorMessage.toString());
+        }
+
+        String email = authDto.getEmail();
+        String password = authDto.getPassword();
+
+        try {
+            authService.authenticate(email, password);
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatus()).body(ex.getReason());
+        }
+
+        String accessToken = jwtTokenizer.generateAccessToken(email);
+        String refreshToken = jwtTokenizer.generateRefreshToken(email);
+
+
+
+        // HttpHeaders 객체를 생성하여 AccessToken과 RefreshToken을 Headers에 추가
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("X-Refresh-Token", refreshToken);
+
+        return ResponseEntity.ok().headers(headers).body("로그인에 성공했습니다");
+    }
+
+    @DeleteMapping("/logouts")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String authorizationHeader) {
+        String accessToken = extractAccessToken(authorizationHeader);
+
+        // 토큰 유효성 검사
+        if (!jwtTokenizer.validateToken(accessToken)) {
+            return ResponseEntity.badRequest().body("Invalid Access Token");
+        }
+
+        // 로그아웃한 토큰을 블랙리스트에 추가
+        tokenBlacklist.add(accessToken);
+
+        return ResponseEntity.ok("Logged out successfully");
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> validateToken(@RequestHeader("Authorization") String authorizationHeader) {
+        String accessToken = extractAccessToken(authorizationHeader);
+
+        // 토큰 유효성 검사 및 블랙리스트 확인
+        if (!jwtTokenizer.validateToken(accessToken) || tokenBlacklist.contains(accessToken)) {
+            return ResponseEntity.badRequest().body("Invalid Access Token");
+        }
+
+        return ResponseEntity.ok("Valid Access Token");
+    }
+
+    private String extractAccessToken(String authorizationHeader) {
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
+
+
---
+++ server/src/main/java/com/main/server/member/MemberController.java
@@ -0,0 +1,126 @@
+package com.main.server.member;
+
+import com.main.server.member.dto.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// CORS Handle
+@CrossOrigin
+@RestController
+public class MemberController {
+    private MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/members")
+    public ResponseEntity<?> postMember(@Valid @RequestBody SignUpDto signUpDto
+            , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // 유효성 검증에 실패한 경우 에러 메시지를 처리하고 응답을 구성한다.
+            StringBuilder errorMessage = new StringBuilder();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMessage.append(error.getField())
+                        .append(": ")
+                        .append(error.getDefaultMessage())
+                        .append("; \n");
+            }
+            // 에러 메시지를 포스트맨으로 보내기 위해 ResponseEntity를 사용한다.
+            return ResponseEntity.badRequest().body(errorMessage.toString());
+        }
+
+        Member member = new Member(signUpDto);
+        Member registeredMember = memberService.registerMember(member);
+
+        ResponseDto responseDto = new ResponseDto(registeredMember);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/members")
+    public List<ResponseDto> getMembers() {
+        List<Member> allMembers = memberService.findAllMembers();
+
+        return allMembers.stream()
+                .map(member -> new ResponseDto(member))
+                .collect(Collectors.toList());
+
+    }
+
+    @GetMapping("/members/{member-Id}")
+    public ResponseDto getMember(@PathVariable("member-Id") long memberId) {
+        Member foundMember = memberService.findMember(memberId);
+
+        return new ResponseDto(foundMember);
+    }
+
+    // 닉네임 변경
+    @PatchMapping("/members/{member-Id}/nickname")
+    public ResponseDto patchMember(@PathVariable("member-Id") long memberId,
+                                   @RequestBody NicknameDto nicknameDto) {
+        Member member = new Member(nicknameDto);
+        Member updateMember = memberService.updateMember(memberId, member);
+
+        return new ResponseDto(updateMember);
+    }
+
+    // 비밀번호 변경
+    @PostMapping("/members/{member-Id}/password")
+    public ResponseEntity<String> changePassword(@PathVariable("member-Id") long memberId,
+                                                 @RequestBody PasswordDto passwordDto) {
+        boolean passwordChanged = memberService.updatePassword(memberId,
+                                                               passwordDto.getPassword(),
+                                                               passwordDto.getNewPassword());
+        if (passwordChanged) {
+            return ResponseEntity.ok("Password changed successfully!");
+        }
+        else {
+            return ResponseEntity.badRequest().body("Failed to change password.");
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/members/{member-Id}")
+    public void deleteMember(@PathVariable("member-Id") long memberId,
+                             @RequestParam("password") String password) {
+        memberService.terminateMember(memberId, password);
+    }
+
+    // 이미지 업로드
+    @PostMapping("/members/{member-Id}/profile-image")
+    public ResponseEntity<String> uploadProfileImage(@PathVariable("member-Id") long memberId,
+                                                     @RequestParam("file") MultipartFile file) {
+        try {
+            memberService.createProfileImage(memberId, file);
+            return ResponseEntity.ok("Profile image uploaded successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload profile image.");
+        }
+    }
+
+    // 이미지 수정
+    @PatchMapping("/members/{member-Id}/profile-image")
+    public ResponseEntity<String> updateProfileImage(@PathVariable("member-Id") long memberId,
+                                                     @RequestParam("file") MultipartFile file) {
+        try {
+            memberService.updateProfileImage(memberId, file);
+            return ResponseEntity.ok("Profile image updated successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("");
+        }
+    }
+
+
+}
