e5317f9a2b5e3c8e62cefea548966ddb6d9bddec
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-12 14:48:02 +0900

    fix : jwt - accessToken 적용

--- client/src/api/members.js
+++ client/src/api/members.js
@@ -1,12 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 // 임시 URL
-const BASE_URL = 'http://localhost:5173/src/moks';
+const BASE_URL = "https://5000-180-230-249-174.ngrok-free.app";
 
 export const loginAPI = async (email, password) => {
   try {
-    const response = await axios.post(`${BASE_URL}/user.json`, { email, password });
+    const response = await axios.post(`${BASE_URL}/auths`, { email, password });
+    console.log(response.data);
+    const accessToken = response.headers.authorization;
+    localStorage.setItem("accessToken", accessToken);
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.error);
+    throw new Error(error.response.error);
   }
 };
