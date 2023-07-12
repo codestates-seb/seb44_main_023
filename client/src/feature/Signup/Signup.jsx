a73bbe4777919738a24c24cda853e93193b34835
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-12 17:18:25 +0900

    fix : 유효성 검사 수정

--- client/src/feature/Signup/Signup.jsx
+++ client/src/feature/Signup/Signup.jsx
@@ -3,18 +3,16 @@ import { signupAPI } from "../../api/members";
 import useSignupStore from "../../store/store.signup";
 
 const Signup = () => {
-  const [signupInfo, setSignupInfo] = useState({
-    email: "",
-    password: "",
-    nickname: "",
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
   const { validation, setValidation } = useSignupStore();
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    const response = await signupAPI(email, password, nickname);
 
     try {
-      const response = await signupAPI(signupInfo);
       console.log("회원가입 성공:", response);
       // 회원가입 성공 후에 필요한 추가 로직 구현
     } catch (error) {
@@ -24,53 +22,52 @@ const Signup = () => {
   };
 
   const handleEmailValidation = (e) => {
-    const emailVal = e.target.value;
+    const email = e.target.value;
     let emailValidationMSG = "";
 
     // 이메일 유효성 검사
-    if (!emailVal) {
+    if (!email) {
       emailValidationMSG = "이메일을 입력해주세요.";
-    } else if (!isValidEmail(emailVal)) {
+    } else if (!isValidEmail(email)) {
       emailValidationMSG = "유효한 이메일 형식이 아닙니다.";
     }
 
-    // 유효성 검사에 통과하면 value를 저장
-    setSignupInfo({ ...signupInfo, email: emailVal });
     // 유효성 검사 메세지 저장
-    setValidation({ email: emailValidationMSG });
+    setValidation({ ...validation, email: emailValidationMSG });
+    setEmail(email);
   };
 
   const handlePasswordValidation = (e) => {
-    const passwordVal = e.target.value;
+    const password = e.target.value;
     let passwordValidation = "";
 
     // 비밀번호 유효성 검사
-    if (!passwordVal) {
-        passwordValidation = "비밀번호를 입력해주세요.";
-    } else if (!isValidPassword(passwordVal)) {
-        passwordValidation =
+    if (!password) {
+      passwordValidation = "비밀번호를 입력해주세요.";
+    } else if (!isValidPassword(password)) {
+      passwordValidation =
         "비밀번호는 영문 대소문자와 숫자의 조합으로 8자 이상이어야 합니다.";
     }
-    
-    // 유효성 검사에 통과하면 value를 저장
-    setSignupInfo({ ...signupInfo, password: passwordVal });
+
     // 유효성 검사 메세지 저장
-    setValidation({ password: passwordValidation });
+    setValidation({ ...validation, password: passwordValidation });
+    // 유효성 검사에 통과하면 value를 저장
+    setPassword(password);
   };
 
   const handleNickNamelValidation = (e) => {
-    const nickNameVal = e.target.value;
+    const nickname = e.target.value;
     let nickNameValidation = "";
 
     // 닉네임 유효성 검사
-    if (!nickNameVal) {
-        nickNameValidation = "닉네임을 입력해주세요.";
+    if (!nickname) {
+      nickNameValidation = "닉네임을 입력해주세요.";
     }
 
     // 유효성 검사에 통과하면 value를 저장
-    setSignupInfo({ ...signupInfo, nickname: nickNameVal });
     // 유효성 검사 메세지 저장
-    setValidation({ nickname: nickNameValidation });
+    setValidation({ ...validation, nickname: nickNameValidation });
+    setNickname(nickname);
   };
 
   const isValidEmail = (email) => {
@@ -90,28 +87,28 @@ const Signup = () => {
           <input
             type="email"
             placeholder="Email"
-            onBlur={handleEmailValidation}
+            onChange={handleEmailValidation}
           />
         </label>
-          {validation.email && <p>{validation.email}</p>}
+        {validation.email && <p>{validation.email}</p>}
         <br />
-          비밀번호:
-          <input
-            type="password"
-            placeholder="Password"
-            onchan={handlePasswordValidation}
-          />
-          {validation.password && <p>{validation.password}</p>}
+        비밀번호:
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={handlePasswordValidation}
+        />
+        {validation.password && <p>{validation.password}</p>}
         <br />
         <label>
           닉네임:
           <input
             type="text"
             placeholder="Nick Name"
-            onBlur={handleNickNamelValidation}
+            onChange={handleNickNamelValidation}
           />
         </label>
-          {validation.nickname && <p>{validation.nickname}</p>}
+        {validation.nickname && <p>{validation.nickname}</p>}
         <br />
         <button type="submit">회원가입</button>
       </form>
@@ -120,3 +117,4 @@ const Signup = () => {
 };
 
 export default Signup;
+
