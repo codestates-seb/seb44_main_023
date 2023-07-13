22a6fe33ab5f949488b108a8a298a0702244c438
yunee <joanaun92@gmail.com>
2023-07-13 20:10:38 +0900

    feat : init folder

---
+++ client/src/components/Group/Group.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/logoSpare.png";
+import UserAvatar from "../../assets/userAvarta.png";
+import { HiMiniMoon } from "react-icons/hi2";
+import { MdOutlineLogout } from "react-icons/md";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import axios from "axios";
+
+const Group = () => {
+
+  return (
+    <div>그룹모달테스트</div>
+  );
+};
+
+
+export default Group;
---
+++ client/src/feature/Home/WeatherImage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styledq from 'styled-components';
+import {weatherTypeToName, getWeatherImage} from "../../utils/weatherTypeToImage"
+
+const WeatherImage = ({ weatherType }) => {
+  
+  let weatherName = "";
+  if (typeof weatherType ==="string"){
+    weatherName = weatherType;
+  }else {
+    weatherName = weatherTypeToName(weatherType);
+  }
+  const weatherImage = getWeatherImage(weatherName);
+
+  return (
+    <HomeWrapper background={weatherImage}  className="blurred" />
+  );
+};
+
+export default WeatherImage;
+
+const HomeWrapper = styled.div`
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  &.blurred {
+     filter: blur(0.5rem);
+  }
+  background-image: url(${props => props.background});
+  top: 0rem;
+  left: 0rem;
+  z-index: -1;
+  `
