3da1333e222be2fdb04bb035ffcfbf579e94d3af
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-14 04:16:14 +0900

    fix : alert 메세지 수정

--- client/src/feature/Signup/OauthSignup.jsx
+++ client/src/feature/Signup/OauthSignup.jsx
@@ -5,13 +5,13 @@ import styled from "styled-components";
 const OauthSignup = () => {
   // 소셜 로그인 구현 필요
   const handleGoogleClick = () => {
-    alert("🚧공사중인 구글 로그인입니다.");
+    alert("🚧공사중인 구글 회원가입입니다.");
   };
   const handleKakaoClick = () => {
-    alert("🚧공사중인 카카오 로그인입니다.");
+    alert("🚧공사중인 카카오 회원가입입니다.");
   };
   const handleNaverClick = () => {
-    alert("🚧공사중인 네이버 로그인입니다.");
+    alert("🚧공사중인 네이버 회원가입입니다.");
   };
   return (
     <Container>
