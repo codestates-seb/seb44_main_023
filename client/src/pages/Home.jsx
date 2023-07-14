b80d4a7786ac3f7c59d0459ee01e8ecd363c056d
yunee <joanaun92@gmail.com>
2023-07-14 14:36:43 +0900

    fix :divide components each files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import { useNavigate } from "react-router-dom"; // react-router-dom에서 useNav
 import smallLogo from "../assets/home/smallLogo.svg";
 import bigLogo from "../assets/home/BigLogo.svg";
 import moveTo from "../assets/home/moveTo.svg";
-import Button from "../components/Button/Button"
 import "../index.css";
-import WeatherWidget from "../components/WeatherWidget/WeatherWidget";
-import { useGetWeatherInfo } from "../store/store.weather";
-import WeatherImage from '../feature/Home/WeatherImage';
+
+import Main from '../feature/Home/Main';
+import DetailedPage from '../feature/Home/DetailedPage';
+import { useGetWeatherInfo } from '../store/store.weather';
+import Button from "../components/Button/Button";
+
 
 const Home = () => {
   const [showDetails, setShowDetails] = useState(false);
@@ -41,184 +43,34 @@ const Home = () => {
     };
   }, []);
   
-  // return (
-  //   <div>
-  //     {showDetails ? (
-  //       <DetailedPageComponent onBack={handleBack} weatherType={3} />
-  //     ) : (
-  //       <MainComponent onClick={handleClick} weatherType={weatherType} />
-  //     )}
-  //   </div>
-  // );
   return (
-    
-       
- <HomeWrapper>
- <MainWrapper>
-   <MainComponent onClick={handleClick} weatherType={weatherType} />
- </MainWrapper>
- <div ref={detailedPageRef}>
-   {showDetails && (
-     <DetailedPageComponentWrapper>
-       <DetailedPageComponent onBack={handleBack} />
-     </DetailedPageComponentWrapper>
-   )}
- </div>
-</HomeWrapper>
-
-
+    <HomeWrapper>
+      <MainWrapper>
+        <Main onClick={handleClick} weatherType={weatherType} />
+      </MainWrapper>
+      <div ref={detailedPageRef}>
+        {showDetails && (
+          <DetailedPageComponentWrapper>
+            <DetailedPage onBack={handleBack} />
+          </DetailedPageComponentWrapper>
+        )}
+      </div>
+    </HomeWrapper>
   );
 };
 export default Home;
 
 
-const MainComponent = ({ onClick , weatherType}) => {
-  const navigate = useNavigate(); // useNavigate 훅을 사용합니다.
-  const handleLoginButtonClick = () => {
-    navigate("/login"); 
-  };
-  const handleSignUpButtonClick = () => {
-    navigate("/signup"); 
-  };
-
-  return (
-    <MainWrapper>
-    <WeatherImage weatherType={weatherType} style={{zIndex:"-1"}}/>
-        <HomeWrapper >
-        <HomeHeader>
-          <SmallLogo src={smallLogo}></SmallLogo>
-          <WeatherWidget scale={0.95} />
-        </HomeHeader>
-        <HomeMain>
-          <HomeContainer>
-            <HomeDivCol>
-              <BigLogo src={bigLogo}></BigLogo>
-              <SloganText>
-                일정과 가계부를 한번에 관리할 수 있습니다.
-              </SloganText>
-            </HomeDivCol>
-            <HomeDivRow>
-              <Button
-                label="Login" // 버튼 Text 지정
-                size="large" // 버튼 사이즈
-                onClick={handleLoginButtonClick} // onClick
-              />
-              <Button
-                label="Sign up" // 버튼 Text 지정
-                size="large" // 버튼 사이즈
-                onClick={handleSignUpButtonClick} // onClick
-              />
-            </HomeDivRow>
-            <div
-              style={{ display: "flex", marginTop: "5rem", justifyContent: "center", alignItem: "center" }}
-            >
-              <MoveTo src={moveTo} onClick={onClick}></MoveTo>
-            </div>
-          </HomeContainer>
-        </HomeMain>
-      </HomeWrapper>
-    </MainWrapper>
-  
-  );
-};
-const DetailedPageComponent = ({ onBack , weatherType}) => {
-  return (
-    <DetailWrapper>
-      <Button
-              label="뒤로 가기" // 버튼 Text 지정
-              size="large" // 버튼 사이즈
-              onClick={onBack} // onClick
-      />
-    
-      <SloganText>상세 설명 페이지</SloganText>
-      <SloganText>서비스에 대한 상세 설명입니다.</SloganText>
-    </DetailWrapper>  
-  );
-};
-
-
-
-
-const slideDownAnimation = keyframes`
-  0% {
-    transform: translateY(-100%);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
-
-const DetailedPageComponentWrapper = styled.div`
-  animation: ${slideDownAnimation} 0.5s ease-in-out;
-  position: absolute;
-  top: 100%;
-  left: 0;
-`;
-
-const DetailWrapper = styled.div`
+const MainWrapper = styled.div`
+  position:absolute;
+  top:0rem;
   width:100%;
   height:100vh;
-  
   background-position: center;
-  border : green solid 5px;
- `
+  border : yellow solid 5px;
+`
 
-//////
 
-const MoveTo = styled.img`
-  width: 6.4rem;
-  height: 3.2rem;
-`
-const SmallLogo = styled.img`
-  width: 15rem;
-  height: 4.5rem;
-`
-const BigLogo = styled.img`
-  width: 38.8rem;
-  height: 9.7rem;
-`
-const SloganText = styled.div`
-  margin-top:2.5rem;
-  font-size: 2.0rem;
-  line-height: 2.7rem;
-  letter-spacing: 0em;
-  color: var(--color-gray-09);
-`
-const HomeDivRow= styled.div`
-  display: flex;
-  flex-direction:row;
-  justify-content: space-between;
-  align-items: flex-start;
-  margin-top: 13.5rem;
- `
-const HomeDivCol= styled.div`
-  display: flex;
-  flex-direction:column;
-  align-items: center;
-`
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction : column;
-  text-align: center;
-  height: 60%;
-`;
-
-const HomeMain = styled.div`
-  display: flex;
-  justify-content:center;
-  align-items: center;
-  height: 90%;
-`;
-const HomeHeader = styled.div`
-  position: relative;;
-
-  display: flex;
-  flex-direction : row;
-  justify-content: space-between;
-  align-items: center;
-  padding-left:7.2rem ;
-  height: 10%;
-`;
 
 const HomeWrapper = styled.div`
   position:absolute;
@@ -228,11 +80,3 @@ const HomeWrapper = styled.div`
   background-position: center;
   border : red solid 5px;
  `
-const MainWrapper = styled.div`
-position:absolute;
-top:0rem;
-width:100%;
-height:100vh;
-background-position: center;
-border : yellow solid 5px;
-`
