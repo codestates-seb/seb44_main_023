a87c44fef1182a0d66b25db3126aba19500b4cf3
wwwwswe <rotey1016@gmail.com>
2023-07-14 15:00:59 +0900

    fix: 인풋 버그 수정

--- client/src/components/Input/PageInput.jsx
+++ client/src/components/Input/PageInput.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const PageInput = ({
   width,
   info,
   size,
+  top,
+  right,
   height,
   fontSize,
   onChange,
+  eyebutton,
+  suffixSize,
+  color,
   ...props
 }) => {
   return (
@@ -20,12 +25,18 @@ const PageInput = ({
         {...props}
       />
       {info && <InputInfo>{info}</InputInfo>}
+      {eyebutton && (
+        <SuffixContainer top={top} right={right} size={suffixSize} color={color}>
+          {eyebutton}
+        </SuffixContainer>
+      )}
     </InputContainer>
   );
 };
 
 const InputContainer = styled.div`
   width: ${(props) => props.width || "auto"};
+  position: relative;
 `;
 
 const InputField = styled.input`
@@ -37,17 +48,37 @@ const InputField = styled.input`
   outline: none;
   transition: border-bottom-color 0.3s ease;
   background-color: transparent;
-  font-size: ${(props) => props.fontSize || "1.4rem"};
+
+  ${(props) =>
+    props.fontSize &&
+    css`
+      font-size: ${props.fontSize}rem !important;
+    `}
 
   &:focus {
     border-bottom-color: var(--color-blue-03);
   }
 `;
 
-const InputInfo = styled.div`
+const InputInfo = styled.p`
   margin-top: 0.8rem;
   font-size: 1.2rem;
   color: var(--color-red-01);
+  word-wrap: break-word;
+`;
+
+const SuffixContainer = styled.button`
+  top: 50px;
+  right: ${(props) => props.right || "30px"};
+  position: absolute;
+  background-color: transparent;
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.size || "inherit"};
+  cursor: pointer;
+
+  :hover {
+    fill: var(--color-blue-04);
+  }
 `;
 
 export default PageInput;
