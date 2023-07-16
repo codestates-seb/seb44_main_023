e357da510fd561a10f506d69df60003ff1a701a6
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-16 19:51:30 +0900

    fix : members.api.js LoginAPI 함수 정리

--- client/src/api/members.api.js
+++ client/src/api/members.api.js
@@ -78,38 +78,12 @@ export const deleteProfileImage = async (memberId) => {
   }
 };
 
-export const loginAPI = async (email, password, navigate) => {
+export const loginAPI = async (email, password) => {
   try {
     const response = await axios.post(`/api/auths`, { email, password });
-    // console.log(response.data);
-    // accessToken
-    const accessToken = response.headers.authorization;
-    localStorage.setItem("accessToken", accessToken);
-
-    // refreshToken
-    const refreshToken = response.headers["x-refresh-token"];
-    localStorage.setItem("refreshToken", refreshToken);
-
-    return response.data;
+    return response;
   } catch (error) {
-    if (error.response) {
-      if (error.response.status === 404) {
-        alert("존재하지 않는 이메일입니다.");
-        // navigate is not a function 오류 뜸
-        // const navigate = useNavigate();
-        // Hook call
-        navigate("/login");
-      } else if (error.response.status === 401) {
-        alert("비밀번호를 다시 확인해주세요.");
-        navigate("/login");
-      } else {
-        alert("관리자에게 문의하세요");
-        navigate("*");
-      }
-    } else {
-      navigate("*");
-      throw new Error(error.response.error);
-    }
+    throw error.response.status
   }
 };
 
