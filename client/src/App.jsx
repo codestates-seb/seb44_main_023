e4474822c911a3a81033e0a7e36cf3470d8bf7e2
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-17 14:30:19 +0900

    fix : login, signup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import ProfilePage from "./pages/ProfilePage";
 import TestWidget from "./pages/TestWidget";
 import ErrorPage from "./pages/ErrorPage";
 import TodoPage from "./pages/TodoPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
@@ -15,8 +17,8 @@ function App() {
         <Route path="/profile/:id" element={<ProfilePage />} />
         <Route path="/weatherWidget" element={<TestWidget />} />
         <Route path="/todo/:groupId" element={<TodoPage />} />
-        <Route path="/login" element={<TodoPage />} />
-        <Route path="/signup" element={<TodoPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<ErrorPage />} />
 
       </Routes>
