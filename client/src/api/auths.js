de5f217b3241a6fa5482fc0c0e6f6aa8e5851631
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-17 16:34:19 +0900

    feat : login, logout api auths로 합침

---
+++ client/src/api/auths.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post(`/api/auths`, { email, password });
+    return response;
+  } catch (error) {
+    throw error.response.status;
+  }
+};
+
+
+export const logout = async (accessToken) => {
+  try {
+
+    const response = await axios.delete("/api/logouts", {
+      headers: {
+        Authorization: accessToken,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error.response.status;
+  }
+};
+
+export default logout;
