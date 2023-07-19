21ccaea9bf89fbf23c60b5d341c17e1acfb67937
wwwwswe <rotey1016@gmail.com>
2023-07-19 21:28:46 +0900

    fix: 로고 이미지 수정

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import Logo from "../../assets/logoSpare1.png";
+import Logo from "../../assets/logoSpare.png";
 import UserAvatar from "../../assets/userAvarta.png";
 import { HiMiniMoon } from "react-icons/hi2";
 import { MdOutlineLogout } from "react-icons/md";
