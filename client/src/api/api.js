b3827750c1eaff922991631769b943fe1cc6ba8f
jina <wlsdk7245@gmail.com>
2023-07-19 14:58:58 +0900

    hotfix: API 주소 변경

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,7 +4,7 @@ export const API = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "/api"
-      : "http://ec2-3-35-11-50.ap-northeast-2.compute.amazonaws.com:8080",
+      : "https://api.planfinity.co.kr",
   headers: {
     "ngrok-skip-browser-warning": true,
   },
