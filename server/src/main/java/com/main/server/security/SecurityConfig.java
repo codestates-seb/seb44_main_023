3b5737fe87cdbe75f4645955c59daa51ea92fc57
whsg0245@gmail.com <whsg0245@gmail.com>
2023-07-19 10:02:02 +0900

    feat : SecurityConfig

--- server/src/main/java/com/main/server/security/SecurityConfig.java
+++ server/src/main/java/com/main/server/security/SecurityConfig.java
@@ -8,31 +8,29 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-//@EnableWebSecurity
-public class SecurityConfig  {
-
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors().configurationSource(corsConfig.corsConfigurationSource())
-//                .and()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .formLogin().disable()
-//                .httpBasic().disable()
-//                .addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtTokenizer, customUserDetailsService))
-//                .authorizeRequests()
-//                .anyRequest().permitAll();
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .formLogin().disable();
+
     }
+}
 
