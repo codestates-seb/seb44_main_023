6a222fd44e127165a67b79086fc49f833503a58b
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-22 05:08:01 +0900

    feat : payments CRUD, 지출 소비 드롭다운

--- client/src/feature/Ledger/LedgerModal/LedgerModal.jsx
+++ client/src/feature/Ledger/LedgerModal/LedgerModal.jsx
@@ -6,14 +6,41 @@ import Button from "../../../components/Button/Button";
 import styled from "styled-components";
 import LedgerCategory from "./LedgerCategory";
 import LedgerAmount from "./LedgerAmount";
-// import Dropdown from "../../../components/Dropdown/Dropdown";
+import Dropdown from "../../../components/Dropdown/Dropdown";
+import LedgerPayments from "./LedgerPayments";
+
+import { readAllInOutcomes } from "../../../api/inoutcomes.api";
 
 const LedgerModal = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const handleModalVisible = () => setIsModalVisible(!isModalVisible);
+  const [menuItems, setMenuItems] = useState([]);
+  const [inOutComesId, setInOutComesName] = useState([]);
+  
+  useEffect(() => {
+    // API에서 메뉴 아이템 가져오기
+    const fetchMenuItems = async () => {
+      try {
+        const response = await readAllInOutcomes();
+        const mappedMenuItems = response.map((response) => ({
+          key: response.in_outcome_id + 1,
+          label: response.in_outcome_name,
+        }));
+        // in_outcome_id저장
+        // const inOutComesIds = mappedMenuItems.map((item) => item.key);
+        // setInOutComesName(inOutComesIds);
+        setMenuItems(mappedMenuItems);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMenuItems();
+  }, []);
+
 
   return (
-    <div>
+    <>
       <Button
         label="모달 열기"
         size="large"
@@ -33,13 +60,24 @@ const LedgerModal = () => {
           </LedgerAddTitle>
           <ModalBar>
             <LeftContent>
-              <Dropdown>드롭다운 자리</Dropdown>
-              <Dropdown>드롭다운 자리</Dropdown>
-              <Defence>
-                <LedgerCategoryWrapper>
+              <Dropdown
+                menu={menuItems}
+                defaultKey={{ key: 1, label: "수입" }}
+                className="dropdown"
+              />
+              <DivLeftLine />
+              <LedgerCategoryContainer>
+                <LedgerInputWrapper>
+                  <LedgerPayments />
+                </LedgerInputWrapper>
+              </LedgerCategoryContainer>
+              <DivLeftLine />
+              <LedgerCategoryContainer>
+                <LedgerInputWrapper>
                   <LedgerCategory />
-                </LedgerCategoryWrapper>
-              </Defence>
+                </LedgerInputWrapper>
+              </LedgerCategoryContainer>
+              <DivRightLine />
             </LeftContent>
             <RightContent>
               <LedgerAmount />
@@ -67,20 +105,20 @@ const LedgerModal = () => {
               style={{
                 backgroundColor: "var(--color-blue-03)",
               }}
-              onClick={console.log("추가")}
+              onClick={() => console.log("히히")}
             />
           </ButtonWrapper>
         </Container>
       </Modal>
-    </div>
+    </>
   );
 };
 
 export default LedgerModal;
 
-const Dropdown = styled.div`
-  font-size: 1.5rem;
-`;
+// const Dropdown = styled.div`
+//   font-size: 1.5rem;
+// `;
 
 const Container = styled.div`
   margin: 3rem 3rem;
@@ -89,7 +127,7 @@ const Container = styled.div`
 
 const ModalBar = styled.div`
   display: grid;
-  grid-template-columns: 3fr 1fr; /* 왼쪽 부분 3칸, 오른쪽 부분 1칸으로 설정 */
+  grid-template-columns: 3.5fr 1fr;
   align-items: center;
   padding: 1rem;
   font-size: 2rem;
@@ -145,13 +183,22 @@ const TextArea = styled.div`
   }
 `;
 
-const Defence = styled.div`
-  width: 100%;
+const LedgerCategoryContainer = styled.div`
+  display: flex;
+  position: relative;
+  width: 20rem;
+  align-items: center;
 `;
 
-const LedgerCategoryWrapper = styled.div`
-  margin: 0.6rem 0rem 0rem 5rem;
+const LedgerInputWrapper = styled.div`
   position: absolute;
   z-index: 1;
 `;
 
+const DivLeftLine = styled.div`
+  border-left: 1px solid var(--color-gray-10);
+`;
+
+const DivRightLine = styled.div`
+  border-left: 1px solid var(--color-gray-10);
+`;
