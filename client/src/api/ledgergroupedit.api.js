7b311f160ae2c5ab3252805cce0dd7367b10a6d3
yunee <joanaun92@gmail.com>
2023-07-23 14:10:34 +0900

    feat : make api file for editing group

---
+++ client/src/api/ledgergroupedit.api.js
@@ -0,0 +1,43 @@
+import { API } from "./api";
+
+export const readLedgerGroups = async () => {
+	try {
+		const res = await API.get(`/ledgergroups`);
+		return res.data;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const updateLedgerGroup = async (
+	memberId,
+	groupId,
+	ledger_group_title
+) => {
+	try {
+		console.log("groupId!:", groupId);
+		console.log("data!:", ledger_group_title);
+
+		const res = await API.patch(`/ledgergroups/${groupId}`, {
+			memberId,
+			groupId,
+			ledger_group_title,
+		});
+		return res.data;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const deleteLedgerGroup = async (groupId) => {
+	console.log("deleteLedgerGroup");
+	console.log("deleteLedgerGroup : groupId!:", groupId);
+
+	try {
+		console.log(" try groupId!:", groupId);
+		await API.delete(`/ledgergroups/${groupId}`);
+		console.log("done");
+	} catch (err) {
+		throw err;
+	}
+};
