747ad3e81c0f1b00fce1c17beaec0e757331b91d
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-26 02:36:34 +0900

    fix : login api

--- client/src/api/auths.api.js
+++ client/src/api/auths.api.js
@@ -1,8 +1,9 @@
 import { API } from "./api";
+import axios from "axios";
 
 export const login = async (email, password) => {
   try {
-    const response = await API.post(`/api/auths`, { email, password });
+    const response = await axios.post(`/api/auths`, { email, password });
     return response;
   } catch (error) {
     throw error.response.status;
