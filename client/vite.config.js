6d175d25aa4489f13e9d222e2f7ff659049725f5
Scarlett0JS <scarlett.js1010@gmail.com>
2023-07-26 02:40:41 +0900

    fix : target

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api.planfinity.co.kr/",
+        target: "https://api.planfinity.co.kr",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
