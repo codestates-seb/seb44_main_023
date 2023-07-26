b83da54683f20775a20bd17b8d6f091ac09aba66
조성건 <124638491+Seonggeon2@users.noreply.github.com>
2023-07-26 23:59:58 +0900

    Update AuthResponse.java

--- server/src/main/java/com/main/server/auth/dto/AuthResponse.java
+++ server/src/main/java/com/main/server/auth/dto/AuthResponse.java
@@ -4,13 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
-
-    public AuthResponse(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
+    private Long memberId;    
+    
+    
 }
