9b7b8ac841d20815f8544272ead187c95982cec6
조성건 <124638491+Seonggeon2@users.noreply.github.com>
2023-07-27 09:45:06 +0900

    Update LedgerGroupService.java

--- server/src/main/java/com/main/server/ledgerGroup/service/LedgerGroupService.java
+++ server/src/main/java/com/main/server/ledgerGroup/service/LedgerGroupService.java
@@ -8,7 +8,7 @@ import com.main.server.ledgerGroup.invitationDto.InvitationLedgerGroupPostDto;
 import java.util.List;
 
 public interface LedgerGroupService {
-    LedgerGroup createLedgerGroup(LedgerGroupPostDto postDto);
+    LedgerGroup createLedgerGroup(LedgerGroup ledgerGroup);
     LedgerGroup updateLedgerGroup(Long ledgerGroupId, LedgerGroupPatchDto patchDto);
     LedgerGroup getLedgerGroup(Long ledgerGroupId);
     List<LedgerGroup> getLedgerGroups();
